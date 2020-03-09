1c9a8b517e7118c1db5c808c7f991eac17941846
Alex Merecka <merecka@gmail.com>
2020-03-09 15:50:58 -0500

    Configured and deployed app to Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCo-SaqrJ_5dVN1rv-bWrSN2gEf7C-qhGQ',
+    authDomain: 'oshop-b28c7.firebaseapp.com',
+    databaseURL: 'https://oshop-b28c7.firebaseio.com',
+    projectId: 'oshop-b28c7',
+    storageBucket: 'oshop-b28c7.appspot.com',
+    messagingSenderId: '616533371738',
+    appId: '1:616533371738:web:b918625b8007a729952163',
+    measurementId: 'G-FYL0MN1ZVZ'
+  }
 };
